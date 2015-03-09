cf4cfb5227456cc229737f60df80b0ce8332e985
Julia Ford <julia.m.ford@gmail.com>
2015-03-09 16:44:02 -0400

    Tile.cs: Document ALL THE THINGS!
    1 file per commit

--- ThePrincessBard/ThePrincessBard/Tile.cs
+++ ThePrincessBard/ThePrincessBard/Tile.cs
@@ -48,18 +48,43 @@ namespace ThePrincessBard
     /// </summary>
     struct Tile
     {
+		/// <summary>
+		/// The texture that this tile will display. This variable is public.
+		/// </summary>
         public Texture2D Texture;
+		/// <summary>
+		/// Indicates whether this tile acts like air, an impassable obstacle,
+		/// a platform that can be passed through in one direction, or a
+		/// climbable surface. This variable is public.
+		/// </summary>
         public TileCollision Collision;
 
+		/// <summary>
+		/// The width of a tile in pixels. Hardcoded to 32.
+		/// </summary>
         public const int Width = 32;
+		/// <summary>
+		/// The height of a tile in pixels. Hardcoded to 32.
+		/// </summary>
         public const int Height = 32;
+		/// <summary>
+		/// The center of a tile, along both x and y axis, measured in pixels.
+		/// Width is hardcoded, so this will always evaluate to 16.
+		/// </summary>
         public const int Center = Width / 2;
 
+		/// <summary>
+		/// A vector containing the width and height of a tile. Since
+		/// <see cref="Width"/> and <see cref="Height"/> are hardcoded, this
+		/// will always evaluate to (32, 32).
+		/// </summary>
         public static readonly Vector2 Size = new Vector2(Width, Height);
 
         /// <summary>
         /// Constructs a new tile.
         /// </summary>
+		/// <param name="collision">indicates if this tile is solid</param>
+		/// <param name="texture">the art for this tile</param>
         public Tile(Texture2D texture, TileCollision collision)
         {
             Texture = texture;
